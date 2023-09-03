5da0351a888a5397b2284613de96f15332480406
Solomon Shalom Lijo <71135230+solomonshalom@users.noreply.github.com>
2023-09-03 20:49:14 +0530

    Hello! Mom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -80,7 +80,7 @@ const IndexPage: React.FC<IndexProps> = ({ stats }) => {
 	return (
 		<Container>
 			<Text as="p" variant="section-heading" mb={3}>
-				Introduction // <Link href="/blog">Blog</Link>
+				Introduction // <Link sx={{ textDecoration: 'none' }} href="/blog">Blog</Link>
 			</Text>
 
 			{/*No idea how, but God, no joke, literally saved the code and deployed! Every Glory is to my Lord and Saviour, Jesus Christ!*/}
