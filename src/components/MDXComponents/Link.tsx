bd114cb479f5fc895210afc8a7aa62c48edd6e8f
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-06-26 15:29:37 +0530

    Hello! Mom

--- src/components/MDXComponents/Link.tsx
+++ src/components/MDXComponents/Link.tsx
@@ -11,7 +11,7 @@ const Link: React.FC = (props) => (
 				},
 
 				"&[href^='/']::after": {
-					content: `' (https://bublr.life' attr(href) ')'`,
+					content: `' (https://chat.justice.rest' attr(href) ')'`,
 					fontSize: 1,
 				},
 
